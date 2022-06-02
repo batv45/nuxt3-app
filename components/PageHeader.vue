7e1bc9bc6b301d0d8ad4676ceef68fc82ec87c50
Batuhan <activ45@hotmail.com>
2022-06-02 20:46:46 +0300

    layout update

---
+++ components/PageHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="bg-white shadow">
+    <div class="px-4 sm:px-6 lg:mx-auto lg:px-8">
+      <div class="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
+        <div class="flex-1 min-w-0">
+          <slot />
+        </div>
+
+        <div class="mt-6 flex space-x-3 md:mt-0 md:ml-4">
+          <slot name="buttons">
+          <button type="button"
+                  class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
+            Add money
+          </button>
+          <button type="button"
+                  class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
+            Send money
+          </button>
+          </slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {CheckCircleIcon, OfficeBuildingIcon} from "@heroicons/vue/solid";
+
+export default {
+  components:{
+    OfficeBuildingIcon,
+    CheckCircleIcon,
+  }
+
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,57 @@
 <template>
-  <div class="max-w-sm mx-auto mt-4">
-    <div class="flex items-center">
-      <img
-        class="w-40 h-40 rounded-full mb-4"
-        height="160"
-        width="160"
-        src="https://avatars.githubusercontent.com/u/38668796?v=4"
-        alt="productfrontenddeveloper"
-      />
-      <a
-        target="blank"
-        class="ml-4 text-yellow-400 dark:text-green-400 font-bold"
-        href="https://github.com/productdevbook"
-        >Follow Me Github</a
-      >
+  <div>
+    <!-- Page header -->
+    <PageHeader>
+      <div class="flex items-center">
+        <img class="hidden h-16 w-16 rounded-full sm:block" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.6&w=256&h=256&q=80" alt="" />
+        <div>
+          <div class="flex items-center">
+            <img class="h-16 w-16 rounded-full sm:hidden" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.6&w=256&h=256&q=80" alt="" />
+            <h1 class="ml-3 text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">Good morning, Emilia Birch</h1>
+          </div>
+          <dl class="mt-6 flex flex-col sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap">
+            <dt class="sr-only">Company</dt>
+            <dd class="flex items-center text-sm text-gray-500 font-medium capitalize sm:mr-6">
+              <OfficeBuildingIcon class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+              Duke street studio
+            </dd>
+            <dt class="sr-only">Account status</dt>
+            <dd class="mt-3 flex items-center text-sm text-gray-500 font-medium sm:mr-6 sm:mt-0 capitalize">
+              <CheckCircleIcon class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400" aria-hidden="true" />
+              Verified account
+            </dd>
+          </dl>
+        </div>
+      </div>
+    </PageHeader>
+    <div class="mt-8">
+      <div class=" mx-auto px-4 sm:px-6 lg:px-8">
+        içerik
+      </div>
     </div>
-    <ul role="list" class="mt-10 space-y-4">
-      <li v-for="item in lists" :key="item.id">
-        <List
-          :link="{
-            id: item.id,
-            title: item.title,
-            url: item.url,
-          }"
-        />
-      </li>
-    </ul>
   </div>
 </template>
 
 <script lang="ts">
+import {CashIcon, CheckCircleIcon, ChevronRightIcon, OfficeBuildingIcon} from "@heroicons/vue/solid";
 import { NuxtLink } from "#components";
-import { link } from "~/types";
+import PageHeader from "~/components/PageHeader.vue";
+
+definePageMeta({
+  layout:'authenticated'
+})
+
 export default {
   components: {
+    PageHeader,
     NuxtLink,
+    OfficeBuildingIcon,
+    CheckCircleIcon,
+    CashIcon,
+    ChevronRightIcon,
   },
   setup() {
-    const lists = ref<link[]>([
-      { id: 1, title: "Pinia Store", url: "pinia" },
-      { id: 1, title: "Modal", url: "modal" },
-      { id: 1, title: "Menu", url: "menu" },
-      { id: 1, title: "New soon...", url: "" },
-    ]);
     return {
-      lists,
     };
   },
 };
